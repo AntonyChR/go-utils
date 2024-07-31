f06b0570b3f66a0ebcbb42d5a7f331e3abcc784a
AntonyChR <antonychr99@gmail.com>
2024-07-31 17:47:11 -0500

    [add] assert tests

--- assert_test.go
+++ assert_test.go
@@ -3,7 +3,13 @@ package utils
 import "testing"
 
 func TestAssert(t *testing.T) {
-
   Assert(true,"the tested value is false", t)
-    
+}
+
+func TestAssertEq(t *testing.T) {
+  AssertEq(true,true,"the tested values ar not equal", t)
+}
+
+func TestAssertNe(t *testing.T) {
+  AssertNe(true,false,"the tested values are equal", t)
 }
