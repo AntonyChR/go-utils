328ef2735bf4493847140eb0af78bf6c12438c5e
AntonyChR <antonychr99@gmail.com>
2024-07-31 15:46:21 -0500

    [add] validations

--- terminal.go
+++ terminal.go
@@ -2,20 +2,40 @@ package utils
 
 import (
 	"bytes"
+	"errors"
 	"os"
 	"os/exec"
+	"strconv"
 )
 
-// Get terminal dimentions
-func GetTermDims() (height, width string, err error) {
+func GetTermDims() (height, width int, err error) {
 	cmd := exec.Command("stty", "size")
 	cmd.Stdin = os.Stdin
 	cmd.Stdin = os.Stdout
 	out, err := cmd.Output()
 	if err != nil {
-		return "", "", err
+		return 0,0, err 
+	}
+
+	if len(out) == 0 {
+		return 0, 0, errors.New("empty output from stty command")
 	}
 	lines := bytes.Split(out, []byte("\n"))
-	splt := bytes.Split(lines[0], []byte(" "))
-	return string(splt[0]), string(splt[1]), err
+	if len(lines) == 0 {
+		return 0, 0, errors.New("unexpected output format from stty command")
+	}
+	
+	parts := bytes.Split(lines[0], []byte(" "))
+
+	height, err = strconv.Atoi(string(parts[0]))
+	if err != nil {
+		return 0,0, err 
+	}
+
+	width, err = strconv.Atoi(string(parts[1]))
+	if err != nil {
+		return 0,0, err 
+	}
+
+	return height, width, nil
 }
