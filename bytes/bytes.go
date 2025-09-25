ec0cdf6da3bd0a1c957ee4f3ad9f9743ec8cefd5
AntonyChR <antonychr99@gmail.com>
2025-09-24 23:35:48 -0500

    simplify convertion

--- bytes/bytes.go
+++ bytes/bytes.go
@@ -1,9 +1,6 @@
 // bytes package provides functions for byte manipulation.
 package bytes
 
-func uint16ToBytes(n uint16)[]byte{
-	p1 := uint8(n >> 8)
-	p2 := uint8(n & 0xFF)
-	return []byte{p1,p2}
+func uint16ToBytes(n uint16) []byte {
+	return []byte{byte(n >> 8), byte(n)}
 }
-
